d9e98d78ba29c5af90b059dec8390c81a51d32ad
dancaprima <dancaprimaa@gmail.com>
2016-11-13 01:29:59 +0700

    First Commit Adding Header

---
+++ config.rb
@@ -0,0 +1,14 @@
+require 'sass-globbing'
+require 'singularitygs'
+require 'breakpoint'
+
+
+project_type= :stand_alone
+http_path= "/"
+sass_dir= "scss"
+images_dir = "public/img"
+css_dir= "public/css"
+line_comments= false
+preferred_syntax = :scss
+output_style = :expanded
+relative_assets = true
